fc70a3f525ea7c72147031528969dbb7f4cd42de
Andrii Topchyi <atopchyi@gmail.com>
2019-06-05 21:35:18 +0300

    Add project files.

---
+++ AirlineSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AirlineSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
